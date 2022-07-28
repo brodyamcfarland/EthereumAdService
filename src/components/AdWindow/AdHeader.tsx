d2337bf663978ff2c54ee2b42b7d0aaa4aa5dfdd
brodyamcfarland <brodyamcfarland@gmail.com>
2022-07-28 10:04:49 -0700

    HyperLink Functionality and Display Fix

--- src/components/AdWindow/AdHeader.tsx
+++ src/components/AdWindow/AdHeader.tsx
@@ -8,20 +8,27 @@ interface Props {
 const AdHeader = ({contract}: Props) => {
   //========AD-UPLOAD ---- SMART CONTRACT FUNCTION==========//
   const [adNameFromSC, setAdNameFromSC] = useState<string>('');
+  const [hyperlinkFromSC, setHyperlinkFromSC] = useState<string>('');
 
   const showAdName = async () => {
     let scName = await contract.getAdName();
     setAdNameFromSC(scName);    
   }
 
+  const loadHyperlink = async () => {
+    let hyperLink = await contract.getHyperlink();
+    setHyperlinkFromSC(hyperLink);    
+  }
+
   useEffect(() => {
     showAdName();
+    loadHyperlink();
   }, [])
 
   return (
     <div className="flex flex-row top-[0px] text-center border-b-[1px] p-1 gap-1 items-center">
         <img src={EASLOGOSIMPLE} alt='easlogo-simple' className="max-h-[70px] max-w-[100px] shadow-sm"/>
-        <p className='pl-2 text-[13px] font-light'>Sponsored by: <span className='pl-1 text-[15px]'>{adNameFromSC}</span></p>
+        <p className='pl-2 text-[13px] font-light'>Sponsored by: <a href={hyperlinkFromSC} className='pl-1 text-[15px]'>{adNameFromSC}</a></p>
         <p className=''></p>
     </div>
   )
